4f0d2d8a0db6c3e4e1fead8f4c3ea14b8dacd22e
Larissa Gonçalves <larissagoncalves.eng@gmail.com>
2022-07-08 17:46:08 -0300

    Máscara de CPF

---
+++ 2625.sql
@@ -0,0 +1,11 @@
+-- POSTGRESQL
+SELECT
+	CONCAT_WS('-', (CONCAT_WS('.', SUBSTRING(cpf FROM 1 FOR 3), SUBSTRING(cpf FROM 4 FOR 3), SUBSTRING(cpf FROM 7 FOR 3))), SUBSTRING(cpf FROM 10 FOR 2)) AS "CPF"
+FROM
+  natural_person
+
+-- SQL SERVER
+SELECT
+	FORMAT((CONVERT(BIGINT, cpf)), '###\.###\.###\-##') AS CPF
+FROM
+  natural_person
