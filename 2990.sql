bce1b464091d3d10b5cb9869a702ce01bcf7b93c
Larissa Gonçalves <larissagoncalves.eng@gmail.com>
2022-07-08 17:47:54 -0300

    CPF Dos Empregados

---
+++ 2990.sql
@@ -0,0 +1,14 @@
+SELECT
+    e.cpf,
+    e.enome,
+    d.dnome
+FROM
+    empregados e
+        INNER JOIN departamentos d
+            ON e.dnumero = d.dnumero
+        LEFT JOIN trabalha t
+            ON e.cpf = t.cpf_emp
+WHERE
+    t.pnumero IS NULL
+ORDER BY
+    e.cpf
