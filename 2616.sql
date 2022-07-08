cf79b4b7e9e7b5410f8cd8ce27224f2146d1ae69
Larissa Gonçalves <larissagoncalves.eng@gmail.com>
2022-07-08 17:44:53 -0300

    Nenhuma Locação

---
+++ 2616.sql
@@ -0,0 +1,11 @@
+SELECT
+    customers.id,
+    customers.name
+FROM
+    customers
+        LEFT JOIN locations
+            ON customers.id = locations.id_customers
+WHERE
+    locations IS NULL
+ORDER BY
+    customers.id
