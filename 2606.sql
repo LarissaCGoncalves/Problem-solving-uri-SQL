2ef5d9b3f2203a8d0e5350b1154bee562b91d4fc
Larissa Gonçalves <larissagoncalves.eng@gmail.com>
2022-07-08 17:41:58 -0300

    Categorias

---
+++ 2606.sql
@@ -0,0 +1,9 @@
+SELECT
+    products.id,
+    products.name
+FROM
+    products
+        INNER JOIN categories
+            ON products.id_categories = categories.id
+WHERE
+    categories.name LIKE 'super%'
